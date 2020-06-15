bae0f98ebf01d2c346a9f0372710c6496e16cd5c
Alexander Scherer <alexscherer@outlook.com>
2020-06-15 12:14:08 +0200

    Change folders and Signup.html

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-//jshint esversion: 6
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const path = require('path');
+
+// const dotenv = require("dotenv");
+// require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/signup.html');
+});
 
 app.listen(PORT, function() {
   console.log('Server started on PORT ' + PORT);
