5bf90f6cca24997fca8291f27495d81593aaa14e
Alexander Scherer <alexscherer@outlook.com>
2020-06-15 17:12:20 +0200

    Add Mailchimp mail subscription

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
 const favicon = require('serve-favicon');
+const { Http2ServerRequest } = require('http2');
+const https = require('https');
+const { env } = require('process');
 
-
-// const dotenv = require("dotenv");
-// require('dotenv').config();
+const dotenv = require("dotenv");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,20 +23,63 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/signup.html');
 });
 
-app.post('/', (req, res) => {
-  
-  let firstName = req.body.firstName;
-  let lastName = req.body.lastName;
-  let email = req.body.email;
-
-  console.log("First Name: " + firstName + ", Last Name: " + lastName + ", eMail: " + email);
-
-}); 
 
 app.listen(PORT, function() {
   console.log('Server started on PORT ' + PORT);
   console.log('Nodejs version is ' + process.version);
   console.log('Press Ctrl+C to quit.');
+
 });
+
+
+app.post('/', (req, res) => {
+  
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log("First Name: " + firstName + ", Last Name: " + lastName + ", eMail: " + email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const listId = process.env.LIST_ID;
+  const apiKey = process.env.API_KEY;
+  const user = process.env.USER;
+  const auth = process.env.AUTH;
+
+  const url = 'https://us10.api.mailchimp.com/3.0/lists/' + listId;
+  const options = {
+    method: "POST",
+    auth: auth
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on('data', function(data){
+      console.log(JSON.parse(data));
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+
+}); 
+
+
+
+
+
 
 
